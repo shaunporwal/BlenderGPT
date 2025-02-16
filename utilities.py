a9518e164cfbbf948f69a44843d15192d232b2b1
Shaun Porwal <shaun.porwal@gmail.com>
2025-02-15 21:27:02 -0500

    Revert back to original to test out

--- utilities.py
+++ utilities.py
@@ -12,29 +12,29 @@ def get_api_key(context, addon_name):
 
 
 def init_props():
-    bpy.types.Scene.llm_chat_history = bpy.props.CollectionProperty(type=bpy.types.PropertyGroup)
-    bpy.types.Scene.llm_model = bpy.props.EnumProperty(
-    name="Language Model",
-    description="Select the Language Model to use",
+    bpy.types.Scene.gpt4_chat_history = bpy.props.CollectionProperty(type=bpy.types.PropertyGroup)
+    bpy.types.Scene.gpt4_model = bpy.props.EnumProperty(
+    name="GPT Model",
+    description="Select the GPT model to use",
     items=[
         ("gpt-4", "GPT-4 (powerful, expensive)", "Use GPT-4"),
         ("gpt-3.5-turbo", "GPT-3.5 Turbo (less powerful, cheaper)", "Use GPT-3.5 Turbo"),
     ],
-    default="gpt-4o",
+    default="gpt-4",
 )
-    bpy.types.Scene.llm_chat_input = bpy.props.StringProperty(
+    bpy.types.Scene.gpt4_chat_input = bpy.props.StringProperty(
         name="Message",
         description="Enter your message",
         default="",
     )
-    bpy.types.Scene.llm_button_pressed = bpy.props.BoolProperty(default=False)
+    bpy.types.Scene.gpt4_button_pressed = bpy.props.BoolProperty(default=False)
     bpy.types.PropertyGroup.type = bpy.props.StringProperty()
     bpy.types.PropertyGroup.content = bpy.props.StringProperty()
 
 def clear_props():
-    del bpy.types.Scene.llm_chat_history
-    del bpy.types.Scene.llm_chat_input
-    del bpy.types.Scene.llm_button_pressed
+    del bpy.types.Scene.gpt4_chat_history
+    del bpy.types.Scene.gpt4_chat_input
+    del bpy.types.Scene.gpt4_button_pressed
 
 def generate_blender_code(prompt, chat_history, context, system_prompt):
     messages = [{"role": "system", "content": system_prompt}]
@@ -47,8 +47,9 @@ def generate_blender_code(prompt, chat_history, context, system_prompt):
     # Add the current user message
     messages.append({"role": "user", "content": "Can you please write Blender code for me that accomplishes the following task: " + prompt + "? \n. Do not respond with anything that is not Python code. Do not provide explanations"})
 
+
     response = openai.ChatCompletion.create(
-        model=context.scene.llm_model,
+        model=context.scene.gpt4_model,
         messages=messages,
         stream=True,
         max_tokens=1500,
